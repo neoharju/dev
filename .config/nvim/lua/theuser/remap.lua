2a91f4dd24bdddde3c5b1c668a383c6a99a9630c
Niklas Harju <niklas.harju [a helsinki.fi>
2025-07-20 20:58:49 +0300

    feat: init remaps

---
+++ .config/nvim/lua/theuser/remap.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+
+-- moving lines/blocks in visual mode with indenting.
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- vertical split explore
+vim.keymap.set("n", "<leader>pv", vim.cmd.Vex)
+-- copy to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- Keep cursor centered
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+-- Make Y behave like C or D
+vim.keymap.set("n", "Y", "y$")
