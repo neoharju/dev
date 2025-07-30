da24fcded2336c8e25a5c2b9132e31207e4e5c0a
Niklas Harju <niklas.harju [a helsinki.fi>
2025-07-30 17:58:19 +0300

    feat: more keybinds

--- .config/nvim/lua/theuser/remap.lua
+++ .config/nvim/lua/theuser/remap.lua
@@ -6,13 +6,23 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- vertical split explore
 vim.keymap.set("n", "<leader>pv", vim.cmd.Vex)
+
 -- copy to system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
--- Keep cursor centered
+
+-- copy file to system keyboard
+vim.keymap.set({ "n", "v" }, "<leader>Y", [[gg"+yG]])
+
+-- keep cursor centered for search
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
--- Make Y behave like C or D
+
+-- make Y behave like C or D
 vim.keymap.set("n", "Y", "y$")
+
 -- quick fix navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+-- join line and keep cursor at place with mark
+vim.keymap.set("n", "J", "mzJ`z")
