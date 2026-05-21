0054d3c3818cb67ee640145527681586364876ef
Niklas Harju <niklas.harju [a helsinki.fi>
2026-05-21 18:24:06 +0300

    feat: add more keymappings

--- .config/nvim/lua/theuser/remap.lua
+++ .config/nvim/lua/theuser/remap.lua
@@ -1,34 +1,54 @@
 vim.g.mapleader = " "
 
+local map = vim.keymap.set
+
 -- moving lines/blocks in visual mode with indenting.
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- vertical split explore
-vim.keymap.set("n", "<leader>pv", vim.cmd.Vex, { desc = "Vertical split explorer" })
+map("n", "<leader>pv", vim.cmd.Vex, { desc = "Vertical split explorer" })
 
 -- copy to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
+map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
 
 -- copy file to system keyboard
-vim.keymap.set({ "n", "v" }, "<leader>Y", [[gg"+yG]], { desc = "Copy file to system keyboard" })
+map({ "n", "v" }, "<leader>Y", [[gg"+yG]], { desc = "Copy file to system keyboard" })
 
 -- keep cursor centered for search
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 -- make Y behave like C or D
-vim.keymap.set("n", "Y", "y$")
+map("n", "Y", "y$")
 
 -- quick fix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+map("n", "<C-k>", "<cmd>cnext<CR>zz")
+map("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 -- join line and keep cursor at place with mark
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line" })
+map("n", "J", "mzJ`z", { desc = "Join line" })
 
 -- delete into black hole register to avoid wiping recent yank reg
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_dd]], { desc = "Delete to black hole register" })
+map({ "n", "v" }, "<leader>d", [["_dd]], { desc = "Delete to black hole register" })
 
 -- Save file
-vim.keymap.set("n", "<leader>s", "<cmd>w<CR>", { desc = "Save file" })
+map("n", "<leader>s", "<cmd>w<CR>", { desc = "Save file" })
+
+-- Movement in insert mode
+map("i", "<C-b>", "<ESC>^i", { desc = "beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "end of line" })
+map("i", "<C-h>", "<Left>", { desc = "left" })
+map("i", "<C-j>", "<Down>", { desc = "down" })
+map("i", "<C-k>", "<Up>", { desc = "up" })
+map("i", "<C-l>", "<Right>", { desc = "right" })
+
+-- Switch window in normal mode
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+
+-- Comment
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
