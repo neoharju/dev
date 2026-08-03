03609e5c255a6f39cd559e29dca7a923a0c951bd
neoharju <neoharju>
2026-08-03 13:23:47 +0300

    feat: add optimize script; export optimize flags for other scripts

---
+++ lib/optimize.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# lib/optimize.sh
+#
+# exports to optimize builds with run
+export CC="gcc"
+export CXX="g++"
+export CFLAGS="-O3 -march=native -flto=auto -DNDEBUG -s"
+export CXXFLAGS="-O3 -march=native -flto=auto -DNDEBUG -s"
+export RUSTFLAGS="-C target-cpu=native -C opt-level=3 -C debuginfo=0 -C strip=symbols -lto=fat"
+
