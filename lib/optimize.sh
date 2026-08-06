657c191c059fe67cc11ac00edf1d0f03cf71ce01
neoharju <neoharju>
2026-08-06 14:48:19 +0300

    refactor: small changes

--- lib/optimize.sh
+++ lib/optimize.sh
@@ -4,7 +4,9 @@
 # exports to optimize builds with run
 export CC="gcc"
 export CXX="g++"
-export CFLAGS="-O3 -march=native -flto=auto -DNDEBUG -s"
-export CXXFLAGS="-O3 -march=native -flto=auto -DNDEBUG -s"
+export CFLAGS="-O3 -march=native -flto=auto -DNDEBUG"
+export CXXFLAGS="-O3 -march=native -flto=auto -DNDEBUG"
+export LDFLAGS="-flto=auto -s"
+
 export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C lto=fat -C debuginfo=0 -C strip=symbols"
 
